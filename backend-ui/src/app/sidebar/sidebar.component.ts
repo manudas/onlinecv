3e40826bb5f8497eb429f87279afaab6f793b201
Manuel <cibermanu@hotmail.com>
2020-05-01 17:48:45 +0100

    Sidebar almost done

--- backend-ui/src/app/sidebar/sidebar.component.ts
+++ backend-ui/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faBars,
+  faTachometerAlt,
+  faTable,
+  faFlagCheckered,
+  faBookmark,
+  faCloud,
+  faBug,
+  faFlask
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faFileAlt
+} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +20,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+
+  faBars = faBars;
+  faTachometerAlt = faTachometerAlt;
+  faFileAlt = faFileAlt;
+  faTable = faTable;
+  faFlagCheckered = faFlagCheckered;
+  faBookmark = faBookmark;
+  faCloud = faCloud;
+  faBug = faBug;
+  faFlask = faFlask;
+
+  sidebar_icon = 'sidebar_icons';
 
   constructor() { }
 
--- backend-ui/src/app/sidebar/sidebar.module.ts
+++ backend-ui/src/app/sidebar/sidebar.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule,
+    NgbModule
   ],
   exports: [SidebarComponent]
 })
