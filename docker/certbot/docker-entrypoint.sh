4a9cde35f786311e82e0d528942564faa17f9f92
Manuel <cibermanu@hotmail.com>
2023-03-10 01:44:57 +0100

    mend

--- docker/certbot/docker-entrypoint.sh
+++ docker/certbot/docker-entrypoint.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 if [ "${1}" == 'certbot' ]; then
-  if [ -z "${DOCKMAIL}" ]; then
+  if [ -z "${email}" ]; then
     echo "ERROR: administrator email is mandatory"
-  elif [ -z "${DOCKDOMAINS}" ]; then
+  elif [ -z "${domain}" ]; then
     echo "ERROR: at least one domain must be specified"
   else
     ## DEBUGGING COMMAND:
-    # exec certbot certonly --verbose --noninteractive --webroot -w /webroot --agree-tos --email="test@manu.link" -d "manu.link" --test-cert --debug-challenges
-    exec certbot certonly --verbose --noninteractive --quiet --webroot -w /var/www/your-domain.com/ --agree-tos --email="${DOCKMAIL}" -d "${DOCKDOMAINS}"
+    if [ "${debug}"  == 'true' ]; then
+        certbot certonly --verbose --noninteractive --webroot -w /webroot --agree-tos --email="${email}" -d "${domain}" --test-cert --debug-challenges
+    #else
+        #certbot certonly --verbose --noninteractive --quiet --webroot -w /webroot --agree-tos --email="${email}" -d "${domain}"
+    fi
+    exec crond -f
   fi
 elif [ "${1}" == 'certbot-renew' ]; then
    exec certbot renew
